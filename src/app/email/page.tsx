ec40a40fd83fbd4905dbe80c13929c39be8f055d
rlaxotn2300 <rlaxotn2300@naver.com>
2024-09-27 22:33:44 +0900

    로그인페이지 그대로 url유지 버셀 env수정

--- src/app/email/page.tsx
+++ src/app/email/page.tsx
@@ -20,7 +20,7 @@ const Email: React.FC = () => {
 
   const handleLogin = async () => {
     try {
-      const response = await axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/login`, {
+      const response = await axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}login`, {
         email,
         password,
       });
